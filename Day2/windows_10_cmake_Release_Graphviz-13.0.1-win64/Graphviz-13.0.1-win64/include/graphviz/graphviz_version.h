1d36d86456cd746cd2982371e2c808c5f8f86875
hibatullah24 <hibatullah.saleh2000@gmail.com>
2025-06-29 17:49:38 +0400

    Add Day2 folder with deep learning code

---
+++ Day2/windows_10_cmake_Release_Graphviz-13.0.1-win64/Graphviz-13.0.1-win64/include/graphviz/graphviz_version.h
@@ -0,0 +1,9 @@
+#pragma once
+#define GVPLUGIN_CONFIG_FILE "config8"
+#define GVPLUGIN_VERSION 8
+#define PACKAGE_BUGREPORT "https://gitlab.com/graphviz/graphviz/-/issues"
+#define PACKAGE_NAME "graphviz"
+#define PACKAGE_STRING "graphviz 13.0.1"
+#define PACKAGE_TARNAME "graphviz"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "13.0.1"
